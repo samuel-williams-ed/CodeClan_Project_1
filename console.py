d05cd6dbad8e8f13c10ad98ae8c14db44ad010fe
Samuel Williams <samuel.williams.ed@gmail.com>
2022-12-16 14:25:50 +0000

    Added models to console for testing

--- console.py
+++ console.py
@@ -1,21 +1,38 @@
 from models.product import Product
+from models.inventory_holding import Inventory_holding
 
-#load test products
+########################## Load Test Objects ############################
+#load test Products
 test_product_1 = Product("Pickaxe",
 "A basic pickaxe, used to mine for ore and break up tough ground.",
 False, "hand-weapon", 2)
-
 test_product_2 = Product("Sword", 
 "A standard issue short sword. Used for stabbing, slashing, and general purpose swording.",
 False, "hand-weapon", 2)
-
 test_product_list = [test_product_1, test_product_2]
 
-# test print all test items
+#load test Manufacturers
+test_manufacturer_1 = {}
+test_manufacturer_2 = {}
+
+#load test Inventory_holdings
+test_inventory_item_1 = Inventory_holding(
+    "Basics Range Pick-axe", 8, 8, test_product_1, test_manufacturer_1)
+test_inventory_item_2 = Inventory_holding(
+    "Basics Range Short Sword", 20, 6, test_product_2, test_manufacturer_2)
+test_inventory_list = [test_inventory_item_1, test_inventory_item_2]
+
+
+
+
+
+########################## Printing ############################
+# general purpose method to print all lists
 def print_list(list):
     for item in list:
         print("")
         print(item.__dict__)
 
 print_list(test_product_list)
+print_list(test_inventory_list)
 
