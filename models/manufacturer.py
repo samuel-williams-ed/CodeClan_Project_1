c3a9b75ed7fe891334261af2edf733f7e7fa46d5
Samuel Williams <samuel.williams.ed@gmail.com>
2022-12-16 23:36:16 +0000

    Manufacturer class updated

--- models/manufacturer.py
+++ models/manufacturer.py
@@ -1,6 +1,10 @@
 class Manufacturer():
-    def __init__(self, input_name, input, wholesale_cost, delivery_fee,  )
-
+    def __init__(self, input_name, input_wholesale_price, input_delivery_fee, input_taking_orders, input_id=None):
+        self.name = input_name
+        self.wholesale_price = input_wholesale_price
+        self.delivery_fee = input_delivery_fee
+        self.taking_orders = input_taking_orders
+        self._id = input_id
 
 
 #  - manufacturer_name
